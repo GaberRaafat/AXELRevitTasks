6e92fc5f97c819a77ec46e257dace9d2039ad708
Gaber Raafat <gaberraffat8@gmail.com>
2025-05-04 20:11:43 +0300

    Task4 - Create Vertical Framing only

--- Task4/FramingWallCommand.cs
+++ Task4/FramingWallCommand.cs
@@ -1,16 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 
-namespace Task1
+namespace Task4
 {
     [Transaction(TransactionMode.Manual)]
     public class FramingWallCommand : IExternalCommand
     {
+        private const double StudSpacing = 2.00;  // spacing in feet
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                var uidoc = commandData.Application.ActiveUIDocument;
+                var doc = uidoc.Document;
+
+                var selectedWallRef = uidoc.Selection.PickObject(ObjectType.Element, "Select a wall");
+
+                var wallElement = doc.GetElement(selectedWallRef) as Wall;
+
+                if (wallElement == null)
+                {
+                    TaskDialog.Show("Error", "Select a valid wall");
+                    return Result.Failed;
+                }
+
+                using (Transaction transaction = new Transaction(doc, "Create Wall Framing"))
+                {
+                    transaction.Start();
+                    CreateFraming(doc, wallElement);
+                    transaction.Commit();
+                }
+
+                TaskDialog.Show("Success", "Wall framing created successfully.");
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+
+        private void CreateFraming(Document doc, Wall wallElement)
+        {
+            var walllocationCurve = wallElement.Location as LocationCurve;
+
+            if (walllocationCurve == null) { return; }
+
+            var wallCurve = walllocationCurve.Curve as Curve;
+
+            // get wall parameters 
+
+            var wallLenght = wallCurve.Length;
+            var wallWidth = wallElement.Width;
+            var wallHeight = wallElement.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble();
+
+            //  get wall base elevation 
+
+            var levelId = wallElement.get_Parameter(BuiltInParameter.WALL_BASE_CONSTRAINT).AsElementId();
+            var baseLevel = doc.GetElement(levelId) as Level;
+            double baseZelevation = baseLevel.Elevation;
+
+            // get wall normal 
+            var wallDir = (wallCurve.GetEndPoint(1) - wallCurve.GetEndPoint(0)).Normalize();
+            var wallNormal = wallDir.CrossProduct(XYZ.BasisZ).Normalize();
+
+            // Create Vertical Studs
+            double StdSpacing = UnitUtils.ConvertToInternalUnits(StudSpacing, UnitTypeId.Feet);
+            int numberOfStud = (int)(wallLenght / StdSpacing);
+
+            for (int i = 1; i <= numberOfStud; i++) 
+            {
+                var distanceOnCurve = i * StdSpacing;
+
+                if (Math.Abs(distanceOnCurve - wallLenght) < 0.01)
+                    continue;
+
+                XYZ studPoint = wallCurve.Evaluate(distanceOnCurve/ wallLenght, true);
+                CreateVerticalStud(doc, studPoint, wallHeight, wallNormal, wallWidth, baseZelevation);
+
+            }
+
+        }
+
+        private void CreateVerticalStud(Document doc, XYZ studPoint, double wallHeight, XYZ wallNormal, double wallWidth, double baseZelevation)
+        {
+            XYZ offsetBasePoint = studPoint + wallNormal * wallWidth * 0.5 + XYZ.BasisZ * baseZelevation;
+            XYZ topPoint = offsetBasePoint + XYZ.BasisZ * wallHeight;
+
+            Line studVrLine = Line.CreateBound(offsetBasePoint, topPoint);
+            CreateModelCurve(doc, studVrLine, wallNormal, offsetBasePoint);
+        }
+
+        private void CreateModelCurve(Document doc, Curve studCurve, XYZ wallNormal, XYZ wallOrigin)
+        {
+            Plane plane = Plane.CreateByNormalAndOrigin(wallNormal, wallOrigin);
+            SketchPlane sketchPlane = SketchPlane.Create(doc, plane);
+            doc.Create.NewModelCurve(studCurve, sketchPlane);
         }
     }
 }
